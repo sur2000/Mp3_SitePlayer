ba89d7af817b71e0ce357cf680933ec87a28ac3b
sur shri <surshri2001@gmail.com>
2022-01-12 20:53:49 +0530

    first commit

---
+++ music-list.js
@@ -0,0 +1,246 @@
+let allMusic = [
+        {
+          name: "Like a Stone",
+          artist: "Audioslave",
+          img: "music-1",
+          src: "music-1"
+        },
+        {
+          name: "The Scientist",
+          artist: "Coldplay",
+          img: "music-2",
+          src: "music-2"
+        },
+        {
+          name: "Wake Me Up",
+          artist: "Avicii",
+          img: "music-3",
+          src: "music-3"
+        },
+        {
+          name: "Afterglow",
+          artist: "Ed Sheeran",
+          img: "music-4",
+          src: "music-4"
+        },
+        {
+          name: "Nobody Else for Me - Radio Edit",
+          artist: "The War Within, Rebekah Giesbrecht",
+          img: "music-5",
+          src: "music-5"
+        },
+        {
+          name: "Funeral",
+          artist: "Phoebe Bridgers",
+          img: "music-6",
+          src: "music-6"
+        }, 
+        {
+            name: "Kaali Ghagri",
+            artist: "Laman",
+            img: "music-7",
+            src: "music-7"
+          }, 
+          {
+            name: "Apocalypse",
+            artist: "Cigarettes After Sex",
+            img: "music-8",
+            src: "music-8"
+          }, 
+          {
+            name: "In Your Atmosphere - Live at the Nokia Theatre, Los Angeles",
+            artist: "John Mayer",
+            img: "music-9",
+            src: "music-9"
+          }, 
+          {
+            name: "Wind of Change",
+            artist: "Scorpians",
+            img: "music-10",
+            src: "music-10"
+          }, 
+          {
+            name: "Happy-From 'Despicable Me 2",
+            artist: "Pharrell Williams",
+            img: "music-11",
+            src: "music-11"
+          }, 
+          {
+            name: "Arntor, A Warrior",
+            artist: "Windir",
+            img: "music-12",
+            src: "music-12"
+          }, 
+          { 
+            name: "My Heart Will Go On-Love Theme", 
+            artist: "Celine Dion", 
+            img: "music-13", 
+            src: "music-13"
+          }, 
+          { 
+            name: "Let Her Go", 
+            artist: "Passenger", 
+            img: "music-14", 
+            src: "music-14"
+          }, 
+          {
+            name: "Akhri Salaam",
+            artist: "The Local Train",
+            img: "music-15",
+            src: "music-15"
+          },
+          {
+            name: "Burn the Rain",
+            artist: "Kurt Cobain",
+            img: "music-16",
+            src: "music-16"
+          },
+          {
+            name: "Mockingbird",
+            artist: "Eminem",
+            img: "music-17",
+            src: "music-17"
+          },
+          {
+            name: "Cherry Wine-Live",
+            artist: "Hozier",
+            img: "music-18",
+            src: "music-18"
+          },
+          {
+            name: "Let's Fall in Love for the Night",
+            artist: "FINNEAS",
+            img: "music-19",
+            src: "music-19"
+          },
+          {
+            name: "Tired of Being Alone",
+            artist: "AI Green" ,
+            img: "music-20",
+            src: "music-20"
+          }, 
+          {
+              name: "Follow Your Fire",
+              artist: "Kodaline",
+              img: "music-21",
+              src: "music-21"
+            }, 
+            {
+              name: "Cloudy Now-Remastered",
+              artist: "Blackfield",
+              img: "music-22",
+              src: "music-22"
+            }, 
+            {
+              name: "Over the Hills and Far Away-1990 Remaster",
+              artist: "Led Zeppelin",
+              img: "music-23",
+              src: "music-23"
+            }, 
+            {
+              name: "Riha",
+              artist: "Anuv Jain",
+              img: "music-24",
+              src: "music-24"
+            }, 
+            {
+              name: "The Dead Flag Blues",
+              artist: "Godspeed You! Black Emperor",
+              img: "music-25",
+              src: "music-25"
+            }, 
+            {
+              name: "Back to You",
+              artist: "Selena Gomez ",
+              img: "music-26",
+              src: "music-26"
+            }, 
+            {
+              name: "Never gonna give you up",
+              artist: "Rick Astley",
+              img: "music-27",
+              src: "music-27"
+            },
+            {
+              name: "Gray- Mob Psycho 100",
+              artist: "Sojou no Hana",
+              img: "music-28",
+              src: "music-28"
+            },
+            {
+              name: "A Waltz for a night",
+              artist: "Julie Delpy",
+              img: "music-29",
+              src: "music-29"
+            },
+            
+            {
+              name: "Mishri",
+              artist: "Anuv Jain",
+              img: "music-31",
+              src: "music-31"
+            },
+            {
+              name: "it's Been A Long, Long Time",
+              artist: "Harry James & His Orchestra",
+              img: "music-32",
+              src: "music-32"
+            }, 
+            {
+                name: "Exist For Love",
+                artist: "AURORA",
+                img: "music-33",
+                src: "music-33"
+              }, 
+              {
+                name: "A Change Of Heart",
+                artist: "The 1975",
+                img: "music-34",
+                src: "music-34"
+              }, 
+              {
+                name: "しわあわせ",
+                artist: "Vaundy",
+                img: "music-35",
+                src: "music-35"
+              }, 
+              {
+                name: "Fade into you",
+                artist: "Mazzy Star",
+                img: "music-36",
+                src: "music-36"
+              }, 
+              {
+                name: "Yune-Dewa",
+                artist: "Yasuaki Shimizu ",
+                img: "music-37",
+                src: "music-37"
+              }, 
+              {
+                name: "People Are Strange",
+                artist: "The Doors ",
+                img: "music-38",
+                src: "music-38"
+              }, 
+              { 
+                name: "Stargazing ft. Justin Jesso", 
+                artist: "Kygo, Justin Jesso", 
+                img: "music-39", 
+                src: "music-39"
+              }, 
+              { 
+                name: "Unfamiliar", 
+                artist: "HRVY x Seeb x Goodboys", 
+                img: "music-40", 
+                src: "music-40"
+              },
+    
+    
+    
+  
+  
+  
+
+
+
+]
